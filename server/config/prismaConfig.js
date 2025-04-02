098e7e0692b10090d5de96f9d6d99989c0b7c758
Arpit Pandey <arpitpandey1801@gmail.com>
2025-04-02 19:31:32 +0530

    Update prismaConfig.js

--- server/config/prismaConfig.js
+++ server/config/prismaConfig.js
@@ -1,4 +1,15 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient({
+  __internal: {
+    engine: {
+      binaryPath: require('path').join(
+        process.cwd(),
+        'node_modules/.prisma/client',
+        'query-engine-rhel-openssl-3.0.x'
+      )
+    }
+  }
+});
 
 export {prisma} 
