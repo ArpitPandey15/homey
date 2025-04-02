95984a8064a7f016734668b196f6688df8aae2e3
Arpit Pandey <arpitpandey1801@gmail.com>
2025-04-02 18:44:05 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://homey-eight.vercel.app//api",
   // timeout: 10 * 1000, // Global timeout
 });
 
